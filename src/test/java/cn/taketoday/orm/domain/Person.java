fd334f0a070be5d5dad43d024cf8f21f08b72696
Harry Yang <i.take.today@gmail.com>
2024-07-08 22:01:43 +0800

    :white_check_mark:

---
+++ src/test/java/cn/taketoday/orm/domain/Person.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 - 2024 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.orm.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * Person entity.
+ *
+ * @author Sam Brannen
+ * @since 4.08
+ */
+@Entity(name = "person")
+@EntityListeners(PersonListener.class)
+public class Person {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+
+  public Person() {
+  }
+
+  public Person(String name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return this.id;
+  }
+
+  protected void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
